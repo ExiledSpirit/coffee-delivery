6a425997074b397a3ee9e1c453507450373799fd
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2023-02-07 13:51:40 -0300

    Adicionando arquivo com a lista de produtos em estoque

--- src/pages/Home/components/ProductList/index.tsx
+++ src/pages/Home/components/ProductList/index.tsx
@@ -1,15 +1,16 @@
 import { Card, CoffeeList } from './styles'
+import { coffeeOptions } from '../../../../shared/coffee-list'
 
 export function ProductList() {
   return (
     <CoffeeList>
-      <Card>
-        <img src="../../../../assets/Americano.png" alt=""></img>
-      </Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      {coffeeOptions.map((coffeeOption) => {
+        return (
+          <Card key={coffeeOption.id}>
+            <img src={coffeeOption.imageUrl} alt="" />
+          </Card>
+        )
+      })}
     </CoffeeList>
   )
 }
