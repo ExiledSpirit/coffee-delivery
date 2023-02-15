5d55c25625898101c33e66c050e9ab8d75951033
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2023-02-15 16:48:31 -0300

    Removendo console.log restante

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,8 @@
-import { useCart } from '../../contexts/cart-context'
 import { CoffeeList } from './components/CoffeeList'
 import { HomeCover } from './components/HomeCover'
 import { CoffeeSelector } from './styles'
 
 export function Home() {
-  const teste = useCart()
-
-  console.log(teste)
-
   return (
     <div>
       <HomeCover />
